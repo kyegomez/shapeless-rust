d4257d25c090ef3af5635dcbad6808fc46bbb34c
Kye <kye@apacmediasolutions.com>
2023-09-16 14:30:05 -0400

    shapeless

--- shapeless/src/lib.rs
+++ shapeless/src/lib.rs
@@ -3,8 +3,37 @@ use std::fmt::Debug;
 
 pub trait Shapeless: Debug + Any {
     fn as_any(&self) -> &dyn Any;
+    fn type_name(&self) -> &'static str;
 }
+
+impl<T: Shapeless> Shapeless for T {
+    fn as_any(&self) -> &dyn Any {
+        self
+    }
+    fn type_name(&self) -> &'static str {
+        std::any::type_name::<T>()
+    }
+}
+
+
 
 pub fn wrapper<T: Shapeless>(arg: T) {
     println("{:?}", arg);
+}
+
+
+pub fn is_type<T: Shapeless>(arg: &dyn Any) -> bool {
+    arg.as_any().is::<T>()
+}
+
+pub fn downcast_ref<T: Shapeless>(arg: &dyn Shapeless) -> Option<&T> {
+    arg.as_any().downcast_ref::<T>()
+}
+
+pub fn convert<T: Shapeless, U: Shapeless>(arg: T) -> Result<U, &'static str> {
+    if let Some(val) = arg.as_any().downcast_ref::<U>() {
+        Ok(*val)
+    } else {
+        Err("Conversion failed")
+    }
 }
